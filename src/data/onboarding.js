de3c77aa277928048767e648d5bad5afcabcb714
kentmzn <99004341+kentmzn@users.noreply.github.com>
2022-02-16 17:59:20 +0800

    Data on Onboarding

---
+++ src/data/onboarding.js
@@ -0,0 +1,38 @@
+export default Data = [
+    {
+        _id: '1',
+        title: 'Create your account',
+        description: 'Users can able to register and login to their accounts',
+        img: require('../assets/images/onboarding-screen1-trans.png')
+    },
+    {
+        _id: '2',
+        title: 'Manage your account',
+        description: 'Users can able to edit their personal information',
+        img: require('../assets/images/onboarding-screen2-trans.png')
+    },
+    {
+        _id: '3',
+        title: 'Privacy & Security',
+        description: 'Securing users personal accounts details',
+        img: require('../assets/images/onboarding-screen3-trans.png')
+    },
+    {
+        _id: '4',
+        title: 'Speech Transcription',
+        description: 'Real-time speech transcription using Google Speech Recognition',
+        img: require('../assets/images/onboarding-screen4-trans.png')
+    },
+    {
+        _id: '5',
+        title: 'Transcription History',
+        description: 'Users can able to view previous transcripts on the Firebase',
+        img: require('../assets/images/onboarding-screen5-trans.png')
+    },
+    {
+        _id: '6',
+        title: 'User Feedback',
+        description: 'Give us a review to improve our mobile application',
+        img: require('../assets/images/onboarding-screen6-trans.png')
+    },
+]
