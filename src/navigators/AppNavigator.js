7e9cdfcf9409e5a167060810d7dbde47cfd42a98
Kenneth Mazon <99004341+kentmzn@users.noreply.github.com>
2022-02-17 21:05:02 +0800

    feat: AppNavigator

--- src/navigators/AppNavigator.js
+++ src/navigators/AppNavigator.js
@@ -2,9 +2,12 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
+import SplashScreen from '../screens/SplashScreen
+import OnboardingScreen from '../screens/OnboardingScreen
 import AuthenticationScreen from '../screens/AuthenticationScreen'
 import TranscriptionScreen from '../screens/TranscriptionScreen'
 import HistoryScreen from '../screens/HistoryScreen'
+
 
 const Stack = createNativeStackNavigator()
 
@@ -12,6 +15,14 @@ const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='transcription'>
+         <Stack.Screen
+          name='splash'
+          component={SplashScreen}
+        />
+        <Stack.Screen
+          name='onboarding'
+          component={OnboardingScreen}
+        />
         <Stack.Screen
           name='authentication'
           component={AuthenticationScreen}
