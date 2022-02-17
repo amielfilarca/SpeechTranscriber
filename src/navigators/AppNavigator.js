3f203248a8ccfdb65ecb4f7fa3c85d3da2d1841a
Kenneth Mazon <99004341+kentmzn@users.noreply.github.com>
2022-02-17 21:05:36 +0800

    feat: AppNavigator

--- src/navigators/AppNavigator.js
+++ src/navigators/AppNavigator.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-import SplashScreen from '../screens/SplashScreen
-import OnboardingScreen from '../screens/OnboardingScreen
+import SplashScreen from '../screens/SplashScreen'
+import OnboardingScreen from '../screens/OnboardingScreen'
 import AuthenticationScreen from '../screens/AuthenticationScreen'
 import TranscriptionScreen from '../screens/TranscriptionScreen'
 import HistoryScreen from '../screens/HistoryScreen'
